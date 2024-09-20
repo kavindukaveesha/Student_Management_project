4e8cbc06a4972229afeac79630131b295c46b811
Kavindu Kaveesha <kavindukaveesha16@gmail.com>
2024-09-20 16:39:25 +0530

    start project

---
+++ Student_Management/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Student_Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.download__10__removebg_preview;
+            pictureBox1.Location = new Point(160, 20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(180, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(96, 132);
+            label1.Name = "label1";
+            label1.Size = new Size(285, 39);
+            label1.TabIndex = 1;
+            label1.Text = "Skills International";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.ImeMode = ImeMode.Disable;
+            groupBox1.Location = new Point(75, 200);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(350, 200);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Login";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightGray;
+            button2.Location = new Point(222, 141);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightGray;
+            button1.Location = new Point(33, 141);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(146, 95);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(170, 28);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.FromArgb(64, 64, 64);
+            label3.Location = new Point(33, 98);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(146, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(170, 28);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.FromArgb(64, 64, 64);
+            label2.Location = new Point(33, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 21);
+            label2.TabIndex = 0;
+            label2.Text = "Username";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightGray;
+            button3.Location = new Point(12, 412);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 5;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(482, 453);
+            Controls.Add(button3);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login-Skills International";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+    }
+}
